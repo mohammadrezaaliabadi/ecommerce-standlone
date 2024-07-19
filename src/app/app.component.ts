65648c7b1430f228a6b368e04aed1996f36414f6
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2024-07-19 17:30:59 +0330

    Update config for provide http client. close #10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './core/nav-bar/nav-bar.component';
+import { ShopComponent } from './shop/shop.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavBarComponent],
+  imports: [CommonModule,RouterOutlet,NavBarComponent,ShopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ecommerce-standlone';
+  title = 'Welcome to Stylish Online Shop';
 }
