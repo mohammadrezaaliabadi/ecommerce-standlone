34a757c2ff5d03928400d941c66acbb71d42a6dd
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2024-07-21 11:42:39 +0330

    update shop component for init ui and iteration product item. close #13

---
+++ src/app/shared/model/shopparams.ts
@@ -0,0 +1,8 @@
+export class ShopParams{
+    brandId = 0;
+    categoryId = 0;
+    pageIndex =1;
+    pageSize =6;
+    sort = "title";
+    search ="";
+}
--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -2,17 +2,19 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from '../shared/model/product';
 import { ShopService } from './shop.service';
+import { ProductItemComponent } from "./product-item/product-item.component";
 
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ProductItemComponent],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss'
 })
 export class ShopComponent implements OnInit {
   products: IProduct[] = [];
   response: any;
+  totalCount = 0;
   constructor(private shopService: ShopService){}
   
   ngOnInit(): void {
